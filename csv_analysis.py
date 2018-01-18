914949801ec4bbbb7d8f4a823a6b55c755128af1
Romain <etn.romain@gmail.com>
2018-01-18 23:10:09 +0100

    Analyse d'un fichier CSV

--- csv_analysis.py
+++ csv_analysis.py
@@ -1,7 +1,13 @@
 import os
 
-def main():
-    pass
+def launch_analysis(data_file):
+    directory    = os.path.dirname(__file__)                    #we get the rigt path.
+    path_to_file = os.path.join(directory, 'data', data_file)   #with this path, we go inside the folder'data'and get the file.
+    
+    with open(path_to_file, 'r') as f:
+        preview = f.readline()                                  #read the first line.
+        
+    print("Yeah! We managedto read the file. Here is a preview: {}".format(preview))
 
 if __name__ == "__main__":
-    main()
+    launch_analysis('current_mps.csv')
