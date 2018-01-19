9efacb9264d919711b1886ec18fb8798b8a67f56
Romain <etn.romain@gmail.com>
2018-01-19 22:33:15 +0100

    Erreurs et exceptions

--- analysis/csv.py
+++ analysis/csv.py
@@ -4,10 +4,11 @@ def launch_analysis(data_file):
     directory    = path.dirname(path.dirname(__file__))      # we get the right path.
     path_to_file = path.join(directory, 'data', data_file)   # with this path, we go inside the folder `data` and get the file.
     
-    with open(path_to_file, 'r') as f:
+    
+    with open(path_to_file,"r") as f:
         preview = f.readline()
-        
-    print("Yeah! We managed to read the file. Here is a preview: {}".format(preview))
+        print("Yeah! We managed to read the file. Here is a preview:")
+        print(preview)
 
 
 if __name__ == "__main__":
--- parite.py
+++ parite.py
@@ -13,11 +13,25 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    import pdb; pdb.set_trace()
-    if args.extension == 'xml':
-        c_an.launch_analysis(args.datafile)
-    elif args.extension == 'csv':
-        x_an.launch_analysis(args.datafile)
+    
+    try:
+        datafile = args.datafile
+        
+        if datafile == None:
+            raise Warning('You must indicate a datafile!')
+        else:
+            try:
+                if args.extension == 'xml':
+                    x_an.launch_analysis(datafile)
+                elif args.extension == 'csv':
+                    c_an.launch_analysis(datafile)
+            except FileNotFoundError as e:
+                print("Ow :( The file was not found. Here is the original message of the exception :", e)
+            finally:
+                print('#################### Analysis is over ####################')
+            
+    except Warning as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
